5f394be6b4f94f75e77d7fa101f8e69d780d05b3
keyontarver11 <ktarver@techlaunch.io>
2018-10-26 17:28:30 -0400

    first commit

---
+++ precache-manifest.35cf3a813a72fa984b73107ad8e1df60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c38505d7834f063bc856",
+    "url": "/static/js/main.c38505d7.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "c38505d7834f063bc856",
+    "url": "/static/css/main.eb47a503.chunk.css"
+  },
+  {
+    "revision": "77433a7dd6f58b001ffd26c3ed2d8aef",
+    "url": "/index.html"
+  }
+];
